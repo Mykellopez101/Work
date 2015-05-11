0b05f616be332148658a9d6424aee8df15da347f
Mykellopez101 <bruteheartache.54@kippnyccp.org>
2015-05-11 13:38:57 -0400

    Update election.py

--- election.py
+++ election.py
@@ -1,9 +1,14 @@
 from random impoprt random
-
 win=0
 
 for i in range (1000):
-  if wins>=2:
+  regions=0
+  randint(1,100)<=87:
+    regions=regions+1
+    randint(1,100)
+  if regions>=2:<=65:
+    regions=regions+1
+    if regions>=2:<=65:
     print "you win the election"
   else:
-    print "You did not win the elctions'
+    print "You did not win the elctions..."
