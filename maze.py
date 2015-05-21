9ed4f0162ecc2e90ccc27c024bd0e1c0a9066c05
Mykellopez101 <bruteheartache.54@kippnyccp.org>
2015-05-21 15:45:19 -0400

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,32 +1,38 @@
-from gopigo import *
-import time
-import math
-set_speed(100)
 
-enc_tgt(1, 1, 18)
-time.sleep(.1)
-fwd()
-time.sleep(3)
-
-def move_forward(feet):
-  mm = feet * 304.8
-  steps = mm/11.34
-  enc_tgt(1, 1, steps)
-  time.sleep(.1)
-  fwd()
-  time.sleep(3 * feet)
-
-def turnright():
-  enc_tgt(1, 0, 14 steps)
-  time.sleep(.1)
-  right()
-  time.sleep(.1)
-  
-def turnleft():
-  enc_tgt(0, 1, 14 steps)
-  time.sleep(.1)
-  left()
-  time.sleep(.1)
-
-move_forward(3)
-turnright()
+from gipigo import 
+ import time 
+ import math 
+ 
+ 
+ set_speed(100) 
+ 
+ 
+ def move_forward(feet): 
+   mm = 304.8 * feet 
+   steps = mm/11.344 
+   enc_tgt(1,1,steps) 
+   time.sleep(.1) 
+   fwd() 
+   time.sleep(4*feet) 
+ 
+ 
+ def turn_right(): 
+   enc_tgt(1,0,15) 
+   time.sleep(.1) 
+   right() 
+   time.sleep(2) 
+ 
+ def turn_left(): 
+   enc_tgt(0,1,15) 
+   time.sleep(.1) 
+   left() 
+   time.sleep(2) 
+  move_forward(4) 
+  turn_right() 
+  move_forward(3) 
+  turn_right() 
+  move_forward(2) 
+  turn_left() 
+  move_forward(1) 
+  turn_left()    
+  move_forward(3) 
